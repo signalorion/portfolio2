a6f2a07802d3ae55e74f1f4663a76e72f7ffacdf
signalorion <masa4@daum.net>
2020-09-15 17:42:20 +0900

    인트로수정

--- script/js.js
+++ script/js.js
@@ -3,26 +3,13 @@ $(function(){
     $(".intro_index li").mouseenter(function(){
         $(this).addClass("on").siblings().removeClass("on");
         
-        var ebg = $(this).children("a").attr("href");
-        console.log(ebg);
+        var liin =$(this).index();
+        console.log(liin);
         
-        $("#intro").css({
-            background: "url(../"+ebg+") no-repeat center/cover"
-        });
-        
-        var lbg = $(this).attr("class");
-        console.log(lbg);
-        
-        var laft =$(".intro_index li.on::after");
-        laft.css({
-            backgroundColor:"red"
-        });
+        $(".bgbox").children().eq(liin).addClass("active").siblings().removeClass("active");
     });////////인트로 각 이름 호버할 시//////////////
+
     
-    $(".intro_index li a").click(function(e){
-        e.preventDefault();
-    });///////////a링크 이동방지////////////////
-    ///근데 이동하긴 해야하는데... 그냥 호버하면 뜰걸 각각 정하는게 맘 편할까...//
     
     
     
