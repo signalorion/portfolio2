9eba74b48984320a59404956a20e1c4409adb5f7
signalorion <masa4@daum.net>
2020-09-18 19:08:49 +0900

    첫 페이지 형태만 잡아둠

--- script/js.js
+++ script/js.js
@@ -1,3 +1,17 @@
+var msg = [
+        "빌 스트리트가 말할 수 있다면",
+        "올랜도",
+        "단지 흑인이라서, 다른 이유는 없다",
+        "바이러스를 이기는 영양제"
+    ];
+
+var writer = [
+        "제임스 볼드윈",
+        "버지니아 울프",
+        "제임스 볼드윈",
+        "김경철, 김해영"
+    ];
+
 $(function(){
     
     $(".intro_index li").mouseenter(function(){
@@ -10,7 +24,10 @@ $(function(){
     });////////인트로 각 이름 호버할 시//////////////
 
     
-    
+    /*새로나온책 포문돌리기*/
+    for(var i=0; i<4; i++){
+        $(".newlist").append('<li><a href="#"><img src="images/books/'+(i+1)+'.jpg" alt="새책"><b>'+msg[i]+'</b><p>'+writer[i]+'</p></a></li>');
+    };
     
     
     
