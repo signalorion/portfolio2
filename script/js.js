9d50991e8de38de8503796a213f8c8bed1753c50
signalorion <masa4@daum.net>
2020-10-05 18:03:34 +0900

    오픈북스페이지만들었다

--- script/js.js
+++ script/js.js
@@ -40,13 +40,13 @@ var recomm_big = [
     "경제 규칙 다시 쓰기"
 ]
 
-var recomm_writer =[
+var recomm_writer = [
     "김시덕",
     "피에르 르메트르",
     "조지프 스티글리츠"
 ]
 
-var recomm_b =[
+var recomm_b = [
     "문헌학자답게 서울 걷기",
     "도스토옙스키를 연상시키는 문학성 넘치는 스릴러",
     "우리가 보고 있는 것은 빙산의 일각이다"
@@ -54,9 +54,9 @@ var recomm_b =[
 
 var recomm_p = [
     "규장각한국학연구소 김시덕(金時徳) 교수의 새 책 『서울 선언』은 좀 의외의 주제를 다룬다. 제목과 부제에서 알 수 있듯이 이 책은 일종의 답사기다. 고문헌학자가 왜 서울 답사에 나섰을까? 그가 걷고 본 서울은 어떤 도시일까?<br>문헌학자가 서울 답사기를 썼다고 하면 아마도 &lt;문화유산 답사&gt;를 떠올릴 것이다. 궁궐과 박물관, 역사 유적을 돌아보겠거니 생각할 만하다. 그러나 이 책에 그런 장소는 등장하지 않는다. 말하자면, 찬란한 문화유산이나, 아픈 근대의 흔적 같은 이야기는 없다. 물론 이 책도 역사를 이야기한다. 그러나 그 시점은 대체로 현재에 가깝다. <br>저자는 주로 &lt;여기도 서울인가?&gt; 싶은 장소들을 걷는다. 그 장소들은 그가 40여 년간 살고 생활했던 곳들이다. 특별할 것 없고 역사가 없어 보이는 곳들을 걸으며 조금은 다른 서울의 역사를 읽어 낸다. 그는 그 장소들을 &lt;무수히 많은 책이 꽃힌 도서관&gt;이라고 칭한다. 그 속에 담긴 이야기들이 어쩌면 진짜 서울의 역사일 것이라고 강조한다.",
-    
+
     "현재 프랑스에서 가장 인기 있는 작가의 한 명인 피에르 르메트르의 신작 소설 『사흘 그리고 한 인생』이 임호경 번역으로 열린책들에서 출간되었다. 르메트르는 프랑스 최고 문학상인 공쿠르상 수상 작가이면서, 영국 추리 작가 협회상도 수상한 희귀한 작가이다. 『사흘 그리고 한 인생』은 공쿠르상 수상작이었던 『오르부아르』(「맨 오브 마스크」라는 제목으로 4월 국내 개봉)에 이어 발표된 소설로, 곧바로 베스트셀러가 되었고 프랑스에서 현재까지 35만 부가 판매되었다. 무대를 다시 현대로 옮겨, 추리 작가, 스릴러 작가로서 르메트르의 진면목을 유감없이 보여주고 있는 작품이다.<br>시골에서 사는 열두 살 소년 앙투안. 그는 우연한 사고로 동네 꼬마를 죽이고 만다. 두려움에 사로잡힌 앙투안은 숲에 꼬마의 시체를 숨긴다. 시체는 결코 발견되지 않았고 아무도 그를 의심하지 않았다 -- 12년이 지나도록. 이제 앙투안은 단 하루도 잊은 적이 없는 그날의 기억에서 슬슬 떠나도 좋은 것일까? 이때 갑자기 그가 그토록 싫어하는 고향에 내려가 보지 않을 수 없는 일이 생기는데......",
-    
+
     "이 책은 단순한 생각에서 출발한다. 오늘날의 경제가 부유한 사람들에게 더 유리한 방식으로 굴러가고 많은 문제들이 이로부터 생겨나는 것이라면, 우리는 모든 사람들에게 더 이로운 쪽으로 경제의 규칙을 다시 고쳐 써야 한다는 것이다. 그래서 오늘날 우리 경제에서 무엇이 잘못되어 있고, 그것을 어떻게 고쳐야 하는지 설명하는 것을 목적으로 하는 이 책은 먼저 경제 현실을 지배하고 있는 현재의 규칙이 어떻게 만들어지고 어떻게 작동하고 있는지를 고찰한 다음, 곧이어 이를 대체할 새로운 규칙을 제시한다.<br>스티글리츠는 불평등을 만들어 내는 오늘날의 경제 구조를 빙산에 빗대 설명한다. 빙산에서 눈에 보이는 부분은 말 그대로 빙산의 일각일 뿐, 수면 아래에는 거대한 구조물이 자리하고 있다는 것이다. 그러나 사람들의 시야에 들어오는 것은 빙산의 일각이다. 빙산의 맨 꼭대기에 생계비를 벌기에는 불충분한 일자리와 불충분한 복지, 불안한 미래 등 우리가 일상적으로 경험하는 불평등한 현실이 보인다. 정치인들은 이 부분을 가장 중요하게 생각한다. 눈에 보이기 때문이고, 유권자들의 관심도 여기에 머물러 때문이다. 그러나 이러한 현상을 일으키는 동인들은 수면 아래 자리하고 있다. 기업 거버넌스, 세제 구조, 국제 무역 및 금융 협정, 거시 경제 정책, 노동법과 노동 시장, 구조적인 차별 등 경제의 틀을 형성하는 규칙들이 수면 아래 감추어진 채 사람들의 시선에 들어오지 않는다. 그러나 정말 중요한 것은 바로 이것이다. 이 감추어져 있는 것들이 바뀌지 않는 한, 현실에서 실제로 일어나는 변화들은 변화랄 것도 없이 아주 미미할 뿐이다. 스티글리츠는 수면 아래서 겉으로 드러나는 현상을 일으키는 이러한 동인들에 집중하며 이를 경제 규칙이라 명명한다. 수면 아래의 빙산이 배를 침몰시키듯이, 중산층을 침몰시키고 있는 것은 바로 이러한 빙산의 중간 구조물, 즉 규칙이다. 이 같은 규칙들로 이루어진 경제 구조 속에서 누군가는 승자가 되고 누군가는 패자가 된다. "
 ]
 
@@ -136,30 +136,78 @@ $(function () {
 
     });
 
-    /*격자무늬 메뉴 호버하면*/
+    /*격자무늬 메뉴 호버하면**************************************/
     $(".sechover").hide();
 
     $(".boxs_wrap section").hover(function () {
         $(this).find(".sechover").stop().fadeIn(200);
     }, function () {
         $(this).find(".sechover").stop().fadeOut(200);
-    });/////////호버///////////////////
-    
+    }); /////////호버///////////////////
+
     /*전집메뉴 클릭시*/
-    $(".collect").click(function(e){
+    $(".collect").click(function (e) {
         e.preventDefault();
         $(".box_popup").fadeIn();
-        $(".newpop_left, .newpop_right").show();
-        $(".ebook_popup").hide().siblings().show();
+        $(".boxpop_left, .boxpop_right").show();
+        $(".boxpopup_wrap").show().siblings().hide();
     });
     /*전자책메뉴 클릭시*/
-    $(".ebook").click(function(e){
+    $(".ebook").click(function (e) {
         e.preventDefault();
         $(".box_popup").fadeIn();
-        $(".newpop_left, .newpop_right").hide();
+        $(".boxpop_left, .boxpop_right").hide();
         $(".ebook_popup").show().siblings().hide();
     });
+    /*세계문학 클릭시*/
+    $(".world").click(function (e) {
+        e.preventDefault();
+        $(".box_popup").fadeIn();
+        $(".boxpop_left, .boxpop_right").hide();
+        $(".world_popup").show().siblings().hide();
+        
+        /*이미지 순환*/
+        $('.world_popimg').bxSlider({
+        auto: true,
+        autoControls: false,
+        stopAutoOnClick: false,
+        pager: false,
+        slideWidth: 600
+    });
+
+    }); ///////세계문학클릭시//////////
+    /*격자무늬 메뉴 호버시 양옆버튼*/
+    $(".boxpop_left").hover(function () {
+        $(this).find("img").attr("src", "images/icons/bk_left_arrow.png");
+
+    }, function () {
+        $(this).find("img").attr("src", "images/icons/left_arrow.png");
+    }); /////////왼쪽버튼 호버시////////////
+
+    /*$(".newpop_left").click(function(){
+        
+    });/////왼쪽버튼 클릭시////////////////*/
+
+
+    $(".boxpop_right").hover(function () {
+        $(this).find("img").attr("src", "images/icons/bk_right_arrow.png");
+
+    }, function () {
+        $(this).find("img").attr("src", "images/icons/right_arrow.png");
+    }); ///////////오른쪽버튼 호버시/////////////
     
+    $(".boxpop_right").click(function () {
+        var boxcont = $(".boxpopup_wrap");
+        boxcont.stop().animate({
+            left: "-100%"
+        }, 600, function () {
+            boxcont.append(boxcont.find("div").first()).css({
+                left: "0"
+            });
+        }); /////////애니메이트///////////
+    }); ////////오른쪽버튼클릭////////////////
+
+
     /*격자메뉴 팝업 닫기 로테이트*/
     $(".boxpop_close").rotate({
         bind: {
@@ -175,30 +223,30 @@ $(function () {
             }
         }
 
-    });////////X로테이트//////////////
+    }); ////////X로테이트//////////////
     /*격자메뉴팝업 닫기*/
-    $(".boxpop_close").click(function(){
+    $(".boxpop_close").click(function () {
         $(".box_popup").fadeOut();
     });
-    
-    /*격자무늬 팝업시 화살표액션*/
-    $(".newpop_right").click(function(){
+/*
+    새로 나온 책 팝업시 화살표액션
+    $(".newpop_right").click(function () {
         var boxcont = $(".boxpopup_wrap");
         boxcont.animate({
-            left:"-100%"
-        },600,function(){
+            left: "-100%"
+        }, 600, function () {
             boxcont.append(boxcont.find("div").first()).css({
-                left:"0"
+                left: "0"
             });
-        });/////////애니메이트///////////
-    });////////오른쪽버튼클릭////////////////
-    
+        }); /////////애니메이트///////////
+    }); ////////오른쪽버튼클릭////////////////*/
+
     //왼쪽버튼은 어케할까??
-    
-    
-    
-    
-    
+
+
+
+
+
 
     /*새로나온 책 양옆버튼*/
     $(".newpop_left").hover(function () {
@@ -206,8 +254,8 @@ $(function () {
 
     }, function () {
         $(this).find("img").attr("src", "images/icons/left_arrow.png");
-    });/////////왼쪽버튼 호버시////////////
-    
+    }); /////////왼쪽버튼 호버시////////////
+
     /*$(".newpop_left").click(function(){
         
     });/////왼쪽버튼 클릭시////////////////*/
@@ -218,7 +266,7 @@ $(function () {
 
     }, function () {
         $(this).find("img").attr("src", "images/icons/right_arrow.png");
-    });///////////오른쪽버튼 호버시/////////////
+    }); ///////////오른쪽버튼 호버시/////////////
 
 
 
@@ -237,36 +285,37 @@ $(function () {
             }
         }
 
-    });////////X로테이트//////////////
-    
+    }); ////////X로테이트//////////////
+
     $(".newpop_close").click(function () {
         $(".newpopup_box").fadeOut(600);
-    });///////////X로 팝업닫기////////////
+    }); ///////////X로 팝업닫기////////////
 
     /*새로나온 책 책사진 클릭하면*/
     $(".newlist li a").click(function () {
-        var newbook =$(this).parent().index();
+        var newbook = $(this).parent().index();
         //console.log(newbook);
-        
+
         $(".newpopup_box").fadeIn();
+        
         $(".newpopup_box").find(".newbook").eq(newbook).show().siblings().hide();
-    });//////////새로나온 책사진 클릭시//////////////
-    
+    }); //////////새로나온 책사진 클릭시//////////////
+
     /*편집자 추천도서 클릭하면*/
-    $(".recomm_li a").click(function(e){
+    $(".recomm_li a").click(function (e) {
         e.preventDefault();
         var recommnum = $(this).parent().index();
         //console.log(recommnum);
-        
+
         $(".editer_recomm").animate({
-            height:"100vh"
-        },600,function(){
+            height: "100vh"
+        }, 600, function () {
             $(".editer_recomm").css({
-                height:"auto"
-            });////////css////////////
-        });//////늘어나는 애니메이트/////////
-        
-        $(".recomm_hidden").empty().append('<img src="images/books/recomm'+(recommnum+1)+'.jpg"><div class="hidden_intro"><big>'+recomm_big[recommnum]+'</big><b class="hidden_writer">저자 <span>'+recomm_writer[recommnum]+'</span></b><p><b>'+recomm_b[recommnum]+'</b>'+recomm_p[recommnum]+'</p></div>')
+                height: "auto"
+            }); ////////css////////////
+        }); //////늘어나는 애니메이트/////////
+
+        $(".recomm_hidden").empty().append('<img src="images/books/recomm' + (recommnum + 1) + '.jpg"><div class="hidden_intro"><big>' + recomm_big[recommnum] + '</big><b class="hidden_writer">저자 <span>' + recomm_writer[recommnum] + '</span></b><p><b>' + recomm_b[recommnum] + '</b>' + recomm_p[recommnum] + '</p></div>')
     });
 
 
